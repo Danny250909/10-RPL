85894e64e7f759ec42c04d700948a76186324909
Aditya Daniel X-RPL <adityadn2009@gmail.com>
2025-09-09 20:32:01 +0700

    Add files via upload

---
+++ 10-RPL/VIDEO/PHP/episode 24 sampai 37/restoran/admin/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page | Aplikasi Restoran</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <h1>Restoran</h1>
+            </div>
+         <col-md-10>
+            <div class="float-right mt-5">Log Out</div>
+         </col-md-10>
+         </div>
+        <div class="row mt-5">
+            <div class="col-md-5">
+                <!-- <h2>Admin Page</h2> -->
+                 <ul class="nav flex-column">
+                    <li class="nav-item"><a class="nav-link" href="#">kategori</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Pelanggan</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Order</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Order Detail</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">User</a></li>
+                 </ul>
+            </div>
+            <div class="col-md-10">
+                    
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <p class="text-center">1980 - copyright@smkn2buduran.com</p>
+            </div>
+        </div>
+        <!-- <h1>Admin Page</h1> -->
+    </div>
+</body>
+</html>
