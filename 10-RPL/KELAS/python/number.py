921918d7ce7538ea180c7506df6c680704c09555
Aditya Daniel X-RPL <adityadn2009@gmail.com>
2025-11-22 09:16:36 +0700

    Add files via upload

--- 10-RPL/KELAS/python/number.py
+++ 10-RPL/KELAS/python/number.py
@@ -1,16 +1,28 @@
-ang1 = 17
-ang2 = 11
-
-print("Angka sebelum ditambah: " + str(ang1))
+# ang1 = 17
+# ang2 = 11
+# print("Angka sebelum ditambah: " + str(ang2))
 
 # Cara 1
-# angka = ang1 + 3
+# angka = ang2 + 6
 # print("Angka setelah ditambah: " + str(angka))
 
 # Cara 2
-ang1 += 3
-print("Angka setelah ditambah: " + str(ang1))
+
+# ang2 += 6
+# print("Angka setelah ditambah: " + str(ang2))
 
 # print("Hasil penjumlahan: " + str(ang1 + ang2))
 # print("Hasil persenan: " + str(ang1 % ang2))
 # print("Hasil perpangkatan: " + str(ang1 ** ang2))
+# print("Hasil pengurangan: " + str(ang1 - ang2))
+# print("Hasil perkalian: " + str(ang1 * ang2))
+# print("Hasil pembagian: " + str(ang1 / ang2))
+# print("Hasil pembulatan ke bawah: " + str(ang1 // ang2))
+# print("Hasil pembulatan ke atas: " + str(round(ang1 / ang2)))
+
+
+
+import math
+
+ang = 9
+print(math.sqrt(ang))
