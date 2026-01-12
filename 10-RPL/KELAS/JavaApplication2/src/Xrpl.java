b832fc19d6b4710e4f83e2140ea98a7c98948961
Aditya Daniel X-RPL <adityadn2009@gmail.com>
2026-01-12 09:48:32 +0700

    Add files via upload

---
+++ 10-RPL/KELAS/JavaApplication2/src/Xrpl.java
@@ -0,0 +1,11 @@
+
+public class Xrpl {
+
+
+    public static void main(String[] args) {
+       System.out.println("Ditzxx. Belajar Netbeans");
+       System.out.println("X-RPL Semester 2");
+       System.out.println("Ditzxx. Ganteng");
+    }
+    
+}
